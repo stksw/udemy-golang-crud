615c1f32c778f92b23769919b416764f8a0d0590
koyoukai <ukai@irsc.jp>
2021-08-16 21:46:33 +0900

    create docker environment

---
+++ go.mod
@@ -0,0 +1,10 @@
+module ambassador
+
+go 1.16
+
+require (
+	github.com/gofiber/fiber/v2 v2.17.0
+	gorm.io/driver/mysql v1.1.2
+	gorm.io/gorm v1.21.13 
+
+)
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"ambassador/src/database"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	database.Connect()
+    database.AutoMigrate()
+
+    app := fiber.New()
+
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.SendString("Hello, Koyo 👋!")
+    })
+
+    app.Listen(":3000")
+}
